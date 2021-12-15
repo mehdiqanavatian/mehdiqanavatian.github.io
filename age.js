49e1299ba5e8582a407019b1384ccef21d1328a7
Mehdi Qanavatian <66205504+mehdiqanavatian@users.noreply.github.com>
2021-12-15 17:35:30 +0330

    Delete age.js

--- age.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    let ageTD = document.getElementById('ageTD');
-    let dob = new Date(1991, 4, 11, 0, 0, 0);
-    let now = Date.now();
-    ageTD.innerHTML = Math.floor((Math.abs(now - dob)) / (1000 * 60 * 60 * 24 * 365));
-}());
-
